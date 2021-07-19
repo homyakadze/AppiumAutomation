a1621cb9fc89e5fb183d446be91405619dde2b79
Alexander <a.v.chumak88@gmail.com>
2021-07-20 02:45:32 +0300

    Lesson 3 - 10 Clearing the input field

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -83,6 +83,19 @@ public class FirstTest
                 5
         );
 
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text, 'Search…')]"),
+                "Java",
+                "Cannot find search input",
+                5
+        );
+
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Cannot find search field",
+                5
+        );
+
         waitForElementAndClick(
                 By.id("org.wikipedia:id/search_close_btn"),
                 "Cannot find X to cancel search",
@@ -183,6 +196,14 @@ public class FirstTest
         return wait.until(
                 ExpectedConditions.invisibilityOfElementLocated(by)
         );
+    }
+
+
+    private WebElement waitForElementAndClear(By by, String error_message, long timeoutInseconds)
+    {
+        WebElement element = waitForElementPresent(by, error_message, timeoutInseconds);
+        element.clear();
+        return element;
     }
 
 }
