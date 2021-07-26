e7f43a23963ae4af78d170c70f77ebbce9201d68
Alexander <a.v.chumak88@gmail.com>
2021-07-26 16:52:14 +0300

    Lesson 4 - 4 Swipe debugging

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -192,10 +192,113 @@ public class FirstTest
                 20
         );
 
-
-
     }
     //Конец теста свайпа статьи
+
+
+    //Тест сохранения статьи, открытия списка и удаления статьи из списка
+
+    @Test
+    public void saveFirstArticleToMyList()
+    {
+            waitForElementAndClick(
+                    By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                    "Cannot find Search Wikipedia input",
+                    5
+            );
+
+            waitForElementAndSendKeys(
+                    By.xpath("//*[contains(@text, 'Search…')]"),
+                    "Java",
+                    "Cannot find search input",
+                    5
+            );
+
+            waitForElementAndClick(
+                    By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
+                    "Cannot find Search Wikipedia input",
+                    5
+            );
+
+            waitForElementPresent(
+                    By.id("org.wikipedia:id/view_page_title_text"),
+                    "Cannot fint article title",
+                    15
+            );
+
+            waitForElementAndClick(
+                    By.xpath("//android.widget.ImageView[@content-desc='More options']"),
+                    "Cannot find button to open article option",
+                    5
+            );
+
+            waitForElementAndClick(
+                    By.xpath("//*[@resource-id='org.wikipedia:id/title']/*[@text='Add to reading list']"),
+                    "Cannot find options to add article to reading list",
+                    5
+            );
+
+            waitForElementAndClick(
+                    By.id("org.wikipedia:id/onboarding_button"),
+                    "Cannot find 'GOT IT' tip overlay",
+                    5
+            );
+
+            waitForElementAndClear(
+                    By.id("org.wikipedia:id/text_input"),
+                    "Cannot find input to set name of article folder",
+                    5
+            );
+
+            String name_of_folder = "Learning programming";
+
+            waitForElementAndSendKeys(
+                    By.id("org.wikipedia:id/text_input"),
+                    name_of_folder,
+                    "Cannot put text into articles folder input",
+                    5
+            );
+
+            waitForElementAndClick(
+                    By.xpath("//*[@text='OK']"),
+                    "Cannot press OK button",
+                    5
+            );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Cannot close article, cannot find X link",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.FrameLayout[@content-desc='My lists']"),
+                "Cannot find navigation button to My List article",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text='" + name_of_folder + "']"),
+                "Cannot find article in My List",
+                5
+        );
+
+
+        swipeElementToLeft(
+                By.xpath("//*[@text='Java (programming language)']"),
+                "Cannot find saved article"
+        );
+
+        waitForElementNotPresent(
+                By.xpath("//*[@text='Java (programming language)']"),
+                "Cannot delete saved article",
+                5
+        );
+
+    }
+
+    //Конец теста сохранения статьи, открытия списка и удаления статьи из списка
+
 
 
     //Методы
@@ -288,6 +391,29 @@ public class FirstTest
             swipeUpQuick();
             ++already_swipes;
         }
+    }
+
+    //Метод, который свайпает элемент влево
+
+    protected void swipeElementToLeft(By by, String error_message)
+    {
+        WebElement element = waitForElementPresent(
+                by,
+                error_message,
+                10);
+        int left_x = element.getLocation().getX();
+        int right_x = left_x + element.getSize().getWidth();
+        int upper_y = element.getLocation().getY();
+        int lower_y = upper_y + element.getSize().getHeight();
+        int middle_y = (upper_y + lower_y) / 2;
+
+        TouchAction action = new TouchAction(driver);
+        action
+                .press(right_x, middle_y)
+                .waitAction(300)
+                .moveTo(left_x, middle_y)
+                .release()
+                .perform();
     }
 
 
