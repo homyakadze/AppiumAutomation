527a346fa116537da6092f971cabeb6e742b4404
Alexander <a.v.chumak88@gmail.com>
2021-07-26 15:23:05 +0300

    Lesson 4 - 2 Swipe till element found

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,10 +1,12 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -154,6 +156,47 @@ public class FirstTest
     //Конец теста
 
 
+    //Тест свайпа статьи
+
+    @Test
+    public void testSwipeArticle()
+    {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find Search Wikipedia input",
+                5
+        );
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text, 'Search…')]"),
+                "Appium",
+                "Cannot find search input",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='Appium']"),
+                "Cannot find Search Wikipedia input",
+                5
+        );
+
+        waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot fint article title",
+                15
+        );
+
+        swipeUpToFindElement(
+                By.xpath("//*[@text='View page in browser']"),
+                "Cannot find the end of the artile",
+                20
+        );
+
+
+
+    }
+    //Конец теста свайпа статьи
+
 
     //Методы
 
@@ -205,6 +248,48 @@ public class FirstTest
         element.sendKeys(value);
         return element;
     }
+
+
+    //Метод свайпа снизу вверх
+
+    protected void swipeUP(int timeOfSwipe)
+    {
+        TouchAction action = new TouchAction(driver);
+        Dimension size = driver.manage().window().getSize();
+        int x = size.width / 2;
+        int start_y = (int) (size.height * 0.8);
+        int end_y = (int) (size.height * 0.2);
+        action
+                .press(x, start_y)
+                .waitAction(timeOfSwipe)
+                .moveTo(x, end_y)
+                .release()
+                .perform();
+    }
+
+
+    //Метод быстрого свайпа
+
+    protected void swipeUpQuick()
+    {
+        swipeUP(200);
+    }
+
+    //Метод свайпа и поиска элемента
+
+    protected void swipeUpToFindElement(By by, String error_message, int max_swipes)
+    {
+        int already_swipes = 0;
+        while (driver.findElements(by).size() == 0){
+            if (already_swipes > max_swipes){
+                waitForElementPresent(by, "Cannot find element by swiping up. \n" + error_message, 0);
+                return;
+            }
+            swipeUpQuick();
+            ++already_swipes;
+        }
+    }
+
 
 
     //Домашняя работа к 3 занятию
