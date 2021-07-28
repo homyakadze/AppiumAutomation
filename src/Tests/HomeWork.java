a0215afa0f404661150e866d8f24672777ef61d5
Alexander <a.v.chumak88@gmail.com>
2021-07-28 15:05:00 +0300

    Lesson 9 - Home work Ex17

--- src/Tests/HomeWork.java
+++ src/Tests/HomeWork.java
@@ -3,6 +3,7 @@ package Tests;
 import lib.CoreTestCase;
 import lib.UI.*;
 import org.junit.Test;
+import org.openqa.selenium.Platform;
 
 public class HomeWork extends CoreTestCase {
 
@@ -69,6 +70,109 @@ public class HomeWork extends CoreTestCase {
         );
     }
     //Finish home work refactoring Ex5
+
+
+    //Start home work Ex17
+
+    private static final String name_of_folder = "Learning programming";
+    private static final String
+            login = "homyakadze",
+            password = "learnqa40";
+
+    @Test
+    public void testSearcTwoArticlesMW() {
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+        SearchPageObject.initSearchInput();
+        SearchPageObject.typeSearchLine("Java");
+        SearchPageObject.clickByArticleWithSubstring("bject-oriented programming language");
+
+        ArticlePageObject ArticlePageObject = new ArticlePageObject(driver);
+        ArticlePageObject.waitForTitleElement();
+        String article_title = ArticlePageObject.getArticleTitle();
+
+        if (Platform.getInstance().isAndroid()){
+            ArticlePageObject.addArticlesToMySaved(name_of_folder);
+        } else {
+            ArticlePageObject.addArticlesToMySaved();
+        }
+        if (Platform.getInstance().isMW()){
+            AuthorizationPageObject Auth = new AuthorizationPageObject(driver);
+            Auth.clickAuthButton();
+            Auth.enterLoginData(login, password);
+            Auth.submitform();
+
+            ArticlePageObject.waitForTitleElement();
+
+            assertEquals("We are not on the same page after login",
+                    article_title,
+                    ArticlePageObject.getArticleTitle()
+            );
+
+            ArticlePageObject.addArticlesToMySaved();
+        }
+
+        ArticlePageObject.closeArticle();
+
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+        SearchPageObject.initSearchInput();
+        SearchPageObject.typeSearchLine("Java");
+        SearchPageObject.clickByArticleWithSubstring("ikimedia list article");
+
+        ArticlePageObject ArticlePageObject = new ArticlePageObject(driver);
+        ArticlePageObject.waitForTitleElement();
+        article_title = ArticlePageObject.getArticleTitle();
+
+        if (Platform.getInstance().isAndroid()){
+            ArticlePageObject.addArticlesToMySaved(name_of_folder);
+        } else {
+            ArticlePageObject.addArticlesToMySaved();
+        }
+        if (Platform.getInstance().isMW()){
+            AuthorizationPageObject Auth = new AuthorizationPageObject(driver);
+            Auth.clickAuthButton();
+            Auth.enterLoginData(login, password);
+            Auth.submitform();
+
+            ArticlePageObject.waitForTitleElement();
+
+            assertEquals("We are not on the same page after login",
+                    article_title,
+                    ArticlePageObject.getArticleTitle()
+            );
+
+            ArticlePageObject.addArticlesToMySaved();
+        }
+
+        ArticlePageObject.closeArticle();
+
+        NavigationUI NavigationUI = new NavigationUI(driver);
+        NavigationUI.clickMyList();
+
+        MyListPageObject MyListPageObject = new MyListPageObjectFactory(driver);
+        MyListPageObject.swipeByArticleToDelete("Java (programming language)");
+
+        if (Platform.getInstance().isIOS()) || Platform.getInstance().isAndroid()){
+            {
+                SearchPageObject.waitForSearchResult("Wikimedia list article");
+                SearchPageObject.clickByArticleWithSubstring("Java version history");
+
+                article_title = ArticlePageObject.getArticleTitle();
+
+                assertEquals(
+                        "We see unexpected title",
+                        "Java version history",
+                        article_title
+                );
+            } else {
+                //Тут мозги уже поплыли от дедлайна и ничего вразумительного не подсказали(
+            }
+        }
+    //Finish home work Ex17
+
+
+
+
+
 
 
     //Home work refactoring EX3
